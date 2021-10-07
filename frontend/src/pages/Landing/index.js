c151b3048948764f650316d502e53df9b95154df
Damian Bhatia <damian.bhatia@gmail.com>
2021-10-06 22:40:34 -0400

    added google icon; added transitions to buttons on hover

--- frontend/src/pages/Landing/index.js
+++ frontend/src/pages/Landing/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 import { Image } from 'react-bootstrap'
 
-import backgroundImage from '../../assets/landingPageBasketball.png'
 import styles from './index.module.css'
+import googleIcon from '../../assets/googleIcon.svg'
 
 /**
  * Landing Page. This is the first page the user sees when connecting to the website
@@ -13,11 +13,17 @@ import styles from './index.module.css'
 const Landing = () => {
     return (
         <div className={styles.container}>
-            <Image className={styles.image} src={backgroundImage}/>
+            <div className={styles.leftContainer}>
+                <h1 className={styles.slogan}>Giving recreational athletes the professional experience.</h1>
+            </div>
+
             <div className={styles.rightContainer}>
                 <h1 className={styles.title}>OpenRun</h1>
                 <button className={styles.signUpBtn}>Sign Up</button>
-                <button className={styles.signUpGoogleBtn}>Sign up with Google</button>
+                <button className={styles.signUpGoogleBtn}>
+                    <Image src={googleIcon} className={styles.icon} />
+                    Sign up with Google
+                </button>
                 <h6 className={styles.subText}>Already have an account? <a className={styles.link} href="#">Sign in</a></h6>
             </div>
         </div>
