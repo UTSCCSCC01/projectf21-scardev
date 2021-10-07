221f01eb03478fb7993b9466c6ce4a80d1d47dcf
SalikChodhary <50224612+SalikChodhary@users.noreply.github.com>
2021-10-07 02:19:21 -0400

    SCAR-23 signup endpoint is complete; user generated in db

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"openrun/db"
+	"os"
+)
+
 func main() {
+	err := db.Init(os.Getenv("MONGO_URI"), os.Getenv("MONGO_DB_NAME"))
+
+	if err != nil {
+		panic(err)
+	}
 	server := New()
 	server.Run()
 }
--- backend/models/user.go
+++ backend/models/user.go
@@ -1,6 +1,10 @@
 package models
 
 import (
+	"context"
+	"openrun/db"
+
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -8,8 +12,51 @@ type User struct {
 	ID primitive.ObjectID `bson:"_id"`
 	FirstName *string `json:"first_name" validate:"required,max=100"`
 	LastName *string `json:"last_name" validate:"required,max=100"`
-	Password *string `validate:"required,min=8"`
+	Password *string `validate:"required"`
 	Email *string `validate:"required"`
 	Phone *string `validate:"required"`
-	UserId string `json:"user_id"`
+	// UserId string `json:"user_id"`
+}
+
+func (u *User) IsExisting() (bool) {
+	filter := bson.D{{"email", u.Email}}
+	var res User
+	dbInstance, err := db.GetDatabase()
+
+	if err != nil {
+		return false
+	}
+
+	collection, err := dbInstance.OpenCollection("Users")
+
+	if err != nil {
+		return false
+	}
+	err = collection.FindOne(context.TODO(), filter).Decode(&res)
+
+	return err == nil
+}
+
+func (u *User)Insert() ([]byte, error) {
+	
+	dbInstance, err := db.GetDatabase()
+
+	if err != nil {
+		return nil, err
+	}
+	collection, err := dbInstance.OpenCollection("Users")
+
+	if err != nil {
+		return nil, err
+	}
+	res, err := collection.InsertOne(context.TODO(), u)
+
+	if err != nil {
+		return nil, err
+	}
+
+	oid := res.InsertedID.(primitive.ObjectID)
+	idAsBytes := oid[:]
+
+	return idAsBytes, nil
 }
