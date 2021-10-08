e7055dc31096364505802ab50300edc8320059fe
bhatiadamian <dabhatia@suncor.com>
2021-10-08 16:06:57 -0400

    SCAR-01 connected Sign In Page to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Landing from './pages/Landing'
 import SignUp from './pages/SignUp'
 import Home from './pages/Home'
+import SignIn from './pages/SignIn'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/" exact component={Landing} />
         <Route path="/signup" component={SignUp} />
         <Route path="/home" component={Home} /> 
+        <Route path="/login" component={SignIn} />
       </Switch>
     </Router>
   )
--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -4,15 +4,12 @@ import { Link, useHistory } from 'react-router-dom'
 import styles from './index.module.css'
 
 /**
- * Sign Up Page. This is the page where the user signs up for an account.
- * @returns Sign Up Page
+ * Sign In Page. This is the page where the user signs into their account.
+ * @returns Sign In Page
  */
-const SignUp = () => {
+const SignIn = () => {
 
     const history = useHistory();
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [phone, setPhone] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -20,14 +17,11 @@ const SignUp = () => {
         e.preventDefault();
 
         const payload = {
-            first_name: firstName,
-            last_name: lastName,
             password: password,
 		    email: email,
-		    phone: phone
         }
 
-        fetch('http://localhost:5000/signup', {
+        fetch('http://localhost:5000/login', {
             method: 'post',
             body: JSON.stringify(payload)
         })
@@ -46,33 +40,17 @@ const SignUp = () => {
             <form onSubmit={handleSubmit} className={styles.form}>
                 <label className={styles.title}>OpenRun</label>
 
-                <div className={styles.formName}>
-                    <div className={styles.firstName}>
-                        <label>First Name</label>
-                        <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
-                    </div>
-
-                    <div>
-                        <label>Last Name</label>
-                        <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
-                    </div>
-                </div>
-
-                <br style={{clear:'both'}} />
-
-                <label>Phone</label>
-                <input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} className={styles.inputLong}/>
                 <label>Email</label>
                 <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className={styles.inputLong}/>
                 <label>Password</label>
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className={styles.inputLong}/>
 
                 <div className={styles.submitContainer}>
-                    <input type="submit" value="Create Account" className={styles.submit}/>
+                    <input type="submit" value="Login" className={styles.submit}/>
                 </div>
 
                 <div className={styles.signIn}>
-                    <h6 className={styles.subText}>Already have an account? <Link to="/login" className={styles.signInLink}>Sign In</Link></h6>
+                    <h6 className={styles.subText}>Don't have an account? <Link to="/signup" className={styles.signInLink}>Sign Up</Link></h6>
                 </div>
             </form>
             </div>
@@ -80,4 +58,4 @@ const SignUp = () => {
     )
 }
 
-export default SignUp
+export default SignIn
