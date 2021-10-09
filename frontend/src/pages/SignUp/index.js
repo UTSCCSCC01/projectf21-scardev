2ddb04aefb2089d2e9c55860742adb63e5ed738b
SalikChodhary <50224612+SalikChodhary@users.noreply.github.com>
2021-10-08 20:08:10 -0400

    Scar 01 (#1)
    * added .gitignore
    
    * initial setup of landing page
    
    * moved css to seperate module for landing page
    
    * added google icon; added transitions to buttons on hover
    
    * added more boilerplate; added test endpoint; added utility jwt functionality
    
    * signup endpoint is complete; user generated in db
    
    * deleted binary; updated gitignore
    
    * added login endpoint; jwt functionality complete; added example protectedendpoint; added env file; refactoring
    
    * added access-control-allow-origin header
    
    * added documentation for login and signup endpoint
    
    * SCAR-01 connected Sign Up Page to Backend; commented database.go functions
    
    * SCAR-01 connected Sign In Page to backend
    
    * add error message on failed login
    
    Co-authored-by: Damian Bhatia <damian.bhatia@gmail.com>
    Co-authored-by: bhatiadamian <dabhatia@suncor.com>

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -32,12 +32,16 @@ const SignUp = () => {
             body: JSON.stringify(payload)
         })
         .then(res => {
+<<<<<<< HEAD
             if (res.status == 200){
                 res.json().then(body => {
                     history.push('/login')
                     return
                 })
             }
+=======
+            history.push('/home');
+>>>>>>> Scar 01 (#1)
         })
     }
 
